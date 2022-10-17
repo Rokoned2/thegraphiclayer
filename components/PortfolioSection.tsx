57e5e4f1bd90e1dd3e20efa2eee9701fa445fa18
Rokoned2 <rokoned2@gmail.com>
2022-10-16 22:53:13 -0500

    some change

--- components/PortfolioSection.tsx
+++ components/PortfolioSection.tsx
@@ -1,11 +1,13 @@
+import Image from "next/image";
 import React from "react";
-import bgSection from "../images/Depositphotos_31330771_S.jpg";
+import williamsScreen from "../images/williamslex.png";
+import unmsmScreen from "../images/unmsm.png";
 
 const PortfolioSection = () => {
   return (
     <section className="bg-[url(../images/Depositphotos_31330771_S.jpg)] bg-cover bg-center">
-      <div className=" w-[96%] max-w-[100rem] mx-auto py-16">
-        <div className="xl:w-1/3 text-center bg-white drop-shadow-lg px-[0.625rem] py-8 ">
+      <div className=" w-[96%] max-w-[100rem] mx-auto py-16 flex items-start">
+        <div className="xl:w-1/3 text-center bg-white drop-shadow-lg px-[0.625rem] py-8 block mr-5 h-auto">
           <h3 className="text-[2rem] font-thin leading-[1.2] mb-6 text-[#46c2ca]">
             Diseñamos y Construimos Cada Sitio Web a la Medida, en Todo Momento.
           </h3>
@@ -19,7 +21,15 @@ const PortfolioSection = () => {
             OBTÉN UNA COTIZACIÓN
           </button>
         </div>
-        <div></div>
+        <div className="flex justify-evenly xl:w-2/3">
+          <div className="w-[13.2rem]">
+            <Image src={williamsScreen} />
+          </div>
+
+          <div className="w-[13.2rem]">
+            <Image src={unmsmScreen} />
+          </div>
+        </div>
       </div>
     </section>
   );
